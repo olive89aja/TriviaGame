d34b10fe71c6e91c66a994e7c66c48d22192d98a
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-08-21 13:15:12 -0500

    reorganized arrays and showing possible answers after questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,92 @@
 let scorePlayer = 0;
-let questionIndex = 0;
+// let questionIndex = 0;
+// let answer = 0;
+// let a1= 0;
+// let a2 = 0;
+// let a3 = 0;
+
 
 
 //Not sure if a1 a2 a3 is going to work for answers. 
 const triviaQuestions = 
-[{q : "In Baseball, who won the 2016 World Series ?", a1 : "Cubs", a2 : "Indians", a3 : "Yankees", answer : a1},
-{q : "In golf, who won the 2019 Masters ? ", a1 : "Billy Horschel", a2 : "Tiger Woods", a3 :"Brooks Koepka", answer : a2},
-{q :"Who won the 2019 Tour de France ?", a1 : "Lance Armstrong", a2 : "Egan Bernal", a3 : "Geraint Thomas", answer : a2},
-{q : "How many times have the Yankees won the World Series ?", a1 : "20", a2 : "31", a3 : "27", answer : a3},
-{q : "Which Formula 1 pilot won the most world championships ?", a1 : "Michael Schumacher", a2 : "Alain Prost", a3 : "Sebastian Vettel", answer : a1},
-{q : "When was the last time the Bears won the Superbowl ?", a1 : "1996", a2 : "2007", a3 : "1986", answer : a3},
-{q : "How many times did Rafael Nadal win the French Open ?", a1 : "8", a2 : "10", a3 : "12", answer : a3},
-{q : "Out of these two legends, who won the most Grand slams ?", a1 : "Sampras", a2 : "Agassi", answer : a1}];
+["In Baseball, who won the 2016 World Series ?",
+"In golf, who won the 2019 Masters ? ",
+"Who won the 2019 Tour de France ?",
+"How many times have the Yankees won the World Series ?",
+"Which Formula 1 pilot won the most world championships ?",
+"When was the last time the Bears won the Superbowl ?",
+"How many times did Rafael Nadal win the French Open ?",
+"Out of these three legends, who won the most Grand slams ?"];
 
+const triviaAnswers = 
+[["Cubs", "Indians", "Yankees"], ["Billy Horschel", "Tiger Woods", "Brooks Koepka"], 
+["Lance Armstrong", "Egan Bernal", "Geraint Thomas"], ["20", "31", "27"], 
+["Michael Schumacher", "Alain Prost", "Sebastian Vettel"], ["1996", "2007", "1986"],
+["8", "10", "12"], ["Federer", "Agassi", "Sampras"] ]
+
+
+const triviaCorrectanswers =
+["Cubs", "Tiger Woods", "Egan Bernal", "27 times", "Michael Schumacher", "1986", "12", "Federer"]
 
 function askQuestion () {
-    if (questionIndex <= (questions.length-1)) {
-        document.querySelector("#question").innerHTML = questions[questionIndex].q;
-    }
+   
 
-    else {
-        document.querySelector ("#score").innerHTML = (`you tried to answer to 8 questions and`& `your final score is`+score);
+    for (let questionIndex = 0; questionIndex < 8; questionIndex++) {
+        
+        var question = $("<h4>");
+   
+        question.attr("sportsquestion", triviaQuestions[questionIndex]);
+    
+        
+        $("#questions").append(question);
+        
+       question.html(triviaQuestions[questionIndex]);
+    }
+    
+    
+}
+
+function possibleAnswers () {
+
+    
+    for (let answersIndex = 0; answersIndex < 8; answersIndex++) {
+        
+        var answerx = $("<p>");
+   
+        answerx.attr("sportsanswers", triviaAnswers[answersIndex]);
+    
+        
+        $("#answers").append(answerx);
+        
+       answerx.html(triviaAnswers[answersIndex]);
+       
     }
 }
 
 
-function contestantScore () {
-document.querySelector ("#score").innerHTML = "Correct answers :"+score;
-}
+
+
+
 
 
 askQuestion();
-contestantScore();
+possibleAnswers();
+$("p").after($(".sportsquestions"));
 
-$(".radio").on("click",function() {
+// function contestantScore () {
+// document.querySelector ("#score").innerHTML = "Correct answers :"+score;
+// }
+// contestantScore();
 
-    userGuess = click;
+// $("#radio").on("click",function() {
 
-if (userGuess = answer) {
-    score++;
-    contestantScore();
-}
+//     userGuess = $(this).text;
 
-askQuestion();
+// if (userGuess = triviaCorrectanswers) {
+//     score++;
+//     contestantScore();
+// }
 
-})
+// askQuestion();
+
+// })
